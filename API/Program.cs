58f9a5d5db0de0b43450566bace292eb931a5c3c
Damaiceno Castro <damaiceno.castro@portmaputo.com>
2023-12-18 11:27:04 +0200

    configuring Cors

--- API/Program.cs
+++ API/Program.cs
@@ -11,13 +11,29 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+
+app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+
+/*app.UseCors(builder => builder
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
+*/ 
+
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
