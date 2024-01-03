fda20fda31c4c8242580dde5b3d4ab4163934e8c
Damaiceno Castro <damaiceno.castro@portmaputo.com>
2024-01-03 16:01:59 +0200

    Adding a DateTime extension to calculate

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using API.Extensions;
 
 namespace API.Entities
 {
@@ -34,5 +35,10 @@ namespace API.Entities
         public string Country { get; set; }
 
         public List<Photo> Photos { get; set; } = new();
+
+        public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
     }
 }
