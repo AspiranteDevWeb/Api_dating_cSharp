c63df73f589f47b173dee91757d69adf10d1bae8
Damaiceno Castro <damaiceno.castro@portmaputo.com>
2024-01-30 14:39:17 +0200

    Implementing the likes repository

--- API/Entities/UserLike.cs
+++ API/Entities/UserLike.cs
@@ -4,6 +4,6 @@ public class UserLike
 {
     public AppUser SourceUser { get; set; }
     public int SourceUserId { get; set; }
-    public AppUser TargerUser { get; set; }
+    public AppUser TargetUser { get; set; }
     public int TargetUserId { get; set; }
 }
